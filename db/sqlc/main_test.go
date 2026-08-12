dd5be49f503e8720e0f24c044c325e7a87fa8f20
Koustav Das <kdkdas8@gmail.com>
2026-08-12 17:25:18 +0530

    add github actions

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgres://postgres:test1234@localhost:5433/banking?sslmode=disable"
+	dbSource = "postgres://postgres:test1234@localhost:5432/banking?sslmode=disable"
 )
 
 var testQueries *Queries
